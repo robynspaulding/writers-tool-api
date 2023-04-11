20352a27aa33c2de521ce05f196a395b6ea1fe90
RobynSpaulding <robynm27@gmail.com>
2023-04-10 20:29:37 -0700

    adds test project to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Project.create(working_title: "Test title", status: "in progress")
