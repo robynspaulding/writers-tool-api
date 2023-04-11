20d01a97f7bb014b22fb0a7ff1f20f71517f68ab
RobynSpaulding <robynm27@gmail.com>
2023-04-10 20:49:04 -0700

    project index, create, show and update completed

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,29 @@
+class ProjectsController < ApplicationController
+
+  def index
+    @projects = Project.all 
+    render json: @projects.as_json
+  end
+
+  def create
+    @project = Project.create(
+      working_title: params[:working_title],
+      status: params[:status],
+    )
+    render json: @project.as_json
+  end
+
+  def show
+    @project = Project.find_by(id: params[:id])
+    render json: @project.as_json
+  end
+
+  def update
+    @project = Project.find_by(id: params[:id])
+    @project.update(
+      working_title: params[:working_title] || @project.working_title,
+      status: params[:status] || @project.status,
+    )
+    render json: @project.as_json
+  end
+end
--- app/views/projects/_project.json.jbuilder
+++ app/views/projects/_project.json.jbuilder
@@ -1,3 +1,5 @@
 json.id project.id
 json.working_title project.working_title
 json.status project.status
+json.created_at project.created_at
+json.updated_at project.updated_at
