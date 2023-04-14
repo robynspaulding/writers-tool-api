093f9c59d55f607be3f43091c8a99f0d72f758cc
RobynSpaulding <robynm27@gmail.com>
2023-04-14 11:31:09 -0700

    cruds out BetaReader controller, adds beta_readers routes, tests each route in HTTPie

--- app/controllers/beta_readers_controller.rb
+++ app/controllers/beta_readers_controller.rb
@@ -1,3 +1,43 @@
 class BetaReadersController < ApplicationController
- 
+  before_action :beta_reader, only: [:show, :update, :destroy]
+  def index
+    @beta_readers = BetaReader.all 
+    render json: @beta_readers.as_json
+  end
+
+  def create
+    @beta_reader = BetaReader.create(
+      user_id: params[:user_id],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      feedback_notes: params[:feedback_notes],
+    )
+    render json: @beta_reader.as_json
+  end
+
+  def show
+    render json: @beta_reader.as_json
+  end
+
+  def update
+    @beta_reader.update(
+
+      first_name: params[:first_name] || @beta_reader.first_name,
+      last_name: params[:last_name] || @beta_reader.last_name,
+      email: params[:email] || @beta_reader.email,
+      feedback_notes: params[:feedback_notes] || @beta_reader.feedback_notes,
+    )
+    render json: @beta_reader.as_json
+  end
+
+  def destroy
+    @beta_reader.destroy
+    render json: {message: "beta_reader successfully destroyed"}
+  end
+
+  private
+    def beta_reader
+      @beta_reader ||= BetaReader.find_by!(id: params.require(:id))
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   resources :worlds
 
   resources :notes
+
+  resources :beta_readers
 end
