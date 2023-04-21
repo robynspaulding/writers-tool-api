5759d409beece2cd7f785eeddbd0188ae96d6185
RobynSpaulding <robynm27@gmail.com>
2023-04-21 10:44:58 -0700

    adds jwt, bcrypt and rack-cors gems to gemfile, creates user model with attributes name, email and password-digest, adds validations to user.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_181655) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_174316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_181655) do
   create_table "projects", force: :cascade do |t|
     t.string "working_title"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
